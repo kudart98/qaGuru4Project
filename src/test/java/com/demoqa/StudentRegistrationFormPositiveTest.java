93f27fe2095a9d0f2cff7558cf57351d8836a4a9
Artem Kudriavtcev <akudriavtcev@vantage.com>
2021-02-15 18:48:58 -0500

    fix second dz

--- src/test/java/com/demoqa/StudentRegistrationFormPositiveTest.java
+++ src/test/java/com/demoqa/StudentRegistrationFormPositiveTest.java
@@ -2,18 +2,17 @@ package com.demoqa;
 
 import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-
 import org.junit.jupiter.api.Test;
-
-
 import java.io.File;
 
 public class StudentRegistrationFormPositiveTest {
-    //test data
+
     String  firstName = "Kolj",
             lastName = "Ivanov",
             email = "mail@gmail.com",
+            gender = "Male",
             mobileNumber = "9213870987",
             subjects = "English",
             address = "SPb, Fontanka 127",
@@ -27,7 +26,7 @@ public class StudentRegistrationFormPositiveTest {
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
-        $x("//label[contains(text(),'Male')]").click();
+        $(byText(gender)).click();
         $("#userNumber").setValue(mobileNumber);
         // date of Birth !!! Calendar pick 15/10/89
         $("#dateOfBirthInput").click();
@@ -38,16 +37,27 @@ public class StudentRegistrationFormPositiveTest {
         $x("//div[contains(@aria-label,'15th')]").click();
         $("#subjectsInput").setValue(subjects).pressEnter();
         // hobbies 2 of 3
-        $x("//label[contains(text(),'Sport')]").click();
-        $x("//label[contains(text(),'Reading')]").click();
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+       // $x("//label[contains(text(),'Sport')]").click();
+       // $x("//label[contains(text(),'Reading')]").click();
         $("#uploadPicture").uploadFile(new File("./src/test/resources/img/img1.jpg"));
         $("#currentAddress").setValue(address);
         $("#react-select-3-input").setValue(state).pressEnter();
         $("#react-select-4-input").setValue(city).pressEnter();
         $("#submit").click();
 
-        // verification section refactor
-        $x("//div[@class='modal-content']").shouldHave(text(firstName + " " + lastName), text(email), text(mobileNumber), text(subjects), text(address), text(state), text(city));
+        // verification section
+        $$(".table-responsive tr").filterBy(text("Student Name")).shouldHave(texts(firstName + " " + lastName));
+        $$(".table-responsive tr").filterBy(text("Student Email")).shouldHave(texts(email));
+        $$(".table-responsive tr").filterBy(text("Gender")).shouldHave(texts(gender));
+        $$(".table-responsive tr").filterBy(text("Mobile")).shouldHave(texts(mobileNumber));
+        $$(".table-responsive tr").filterBy(text("Date of Birth")).shouldHave(texts("15 October,1989"));
+        $$(".table-responsive tr").filterBy(text("Subjects")).shouldHave(texts(subjects));
+        $$(".table-responsive tr").filterBy(text("Hobbies")).shouldHave(texts("Sports, Reading"));
+        $$(".table-responsive tr").filterBy(text("Picture")).shouldHave(texts("img1.jpg"));
+        $$(".table-responsive tr").filterBy(text("Address")).shouldHave(texts(address));
+        $$(".table-responsive tr").filterBy(text("State and City")).shouldHave(texts(state + " " + city));
 
     }
 
